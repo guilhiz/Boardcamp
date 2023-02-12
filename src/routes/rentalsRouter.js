c84b951d2e54539f66d5392d1840aa4a6dec9826
guilhiz <guilhermevilelasobral@gmail.com>
2023-02-12 15:37:16 -0300

    feat: add validation for new rental data using joi schema

--- src/routes/rentalsRouter.js
+++ src/routes/rentalsRouter.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { rentalFinishedMiddleware } from "../middlewares/rentalMiddleware.js";
+import { validateSchema } from "../middlewares/schemaMIddleware.js";
+import { rentalSchema } from "../schemas/rentalSchema.js";
 import { getRentals, newRental, rentalFinished, deleteRental } from "../controllers/rentalsController.js";
 
 const rentalsRouter = Router();
 
 rentalsRouter.get("/rentals", getRentals);
 
-rentalsRouter.post("/rentals", newRental);
+rentalsRouter.post("/rentals", validateSchema(rentalSchema), newRental);
 
 rentalsRouter.post("/rentals/:id/return", rentalFinishedMiddleware, rentalFinished);
 
