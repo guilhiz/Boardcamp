a813ed9903cec89f80d78b22444f01735409555f
guilhiz <guilhermevilelasobral@gmail.com>
2023-02-12 15:20:42 -0300

    feat: add validation for new game data using joi schema

--- src/routes/gamesRouter.js
+++ src/routes/gamesRouter.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
+import { validateSchema } from "../middlewares/schemaMIddleware.js";
+import { gamesSchema } from "../schemas/gameSchema.js";
 import { getGames, newGames } from "../controllers/gamesController.js";
 
 const gamesRouter = Router()
 
 gamesRouter.get("/games", getGames)
 
-gamesRouter.post("/games", newGames)
+gamesRouter.post("/games",validateSchema(gamesSchema), newGames)
 
 export default gamesRouter
